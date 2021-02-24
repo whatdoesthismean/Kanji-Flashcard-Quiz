dfe97383d714ff0e3c53b3a4a0c2b7cde953b326
whatdoesthismean <75227382+whatdoesthismean@users.noreply.github.com>
2021-02-23 20:02:24 -0800

    Named all controls

--- 日本語フラッシュカード/Form1.Designer.vb
+++ 日本語フラッシュカード/Form1.Designer.vb
@@ -22,129 +22,129 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Button5 = New System.Windows.Forms.Button()
+        Me.lblKanji = New System.Windows.Forms.Label()
+        Me.lblHiragana = New System.Windows.Forms.Label()
+        Me.btnFlipNihongo = New System.Windows.Forms.Button()
+        Me.btnFlipEigo = New System.Windows.Forms.Button()
+        Me.lblEigo = New System.Windows.Forms.Label()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtNihongo = New System.Windows.Forms.TextBox()
+        Me.txtEigo = New System.Windows.Forms.TextBox()
+        Me.btnPrevious = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Label1
+        'lblKanji
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(82, 34)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(177, 135)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "私"
+        Me.lblKanji.AutoSize = True
+        Me.lblKanji.Font = New System.Drawing.Font("Microsoft Sans Serif", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblKanji.Location = New System.Drawing.Point(82, 34)
+        Me.lblKanji.Name = "lblKanji"
+        Me.lblKanji.Size = New System.Drawing.Size(177, 135)
+        Me.lblKanji.TabIndex = 0
+        Me.lblKanji.Text = "私"
         '
-        'Label2
+        'lblHiragana
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 28.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(95, 333)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(138, 55)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "わたし"
+        Me.lblHiragana.AutoSize = True
+        Me.lblHiragana.Font = New System.Drawing.Font("Microsoft Sans Serif", 28.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHiragana.Location = New System.Drawing.Point(95, 333)
+        Me.lblHiragana.Name = "lblHiragana"
+        Me.lblHiragana.Size = New System.Drawing.Size(138, 55)
+        Me.lblHiragana.TabIndex = 1
+        Me.lblHiragana.Text = "わたし"
         '
-        'Button1
+        'btnFlipNihongo
         '
-        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(100, 193)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(144, 34)
-        Me.Button1.TabIndex = 2
-        Me.Button1.Text = "Flip hiragana"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnFlipNihongo.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnFlipNihongo.Location = New System.Drawing.Point(100, 193)
+        Me.btnFlipNihongo.Name = "btnFlipNihongo"
+        Me.btnFlipNihongo.Size = New System.Drawing.Size(144, 34)
+        Me.btnFlipNihongo.TabIndex = 2
+        Me.btnFlipNihongo.Text = "Flip Hiragana"
+        Me.btnFlipNihongo.UseVisualStyleBackColor = True
         '
-        'Button2
+        'btnFlipEigo
         '
-        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button2.Location = New System.Drawing.Point(100, 406)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(144, 34)
-        Me.Button2.TabIndex = 3
-        Me.Button2.Text = "Flip English"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnFlipEigo.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnFlipEigo.Location = New System.Drawing.Point(100, 406)
+        Me.btnFlipEigo.Name = "btnFlipEigo"
+        Me.btnFlipEigo.Size = New System.Drawing.Size(144, 34)
+        Me.btnFlipEigo.TabIndex = 3
+        Me.btnFlipEigo.Text = "Flip English"
+        Me.btnFlipEigo.UseVisualStyleBackColor = True
         '
-        'Label3
+        'lblEigo
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 28.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(150, 513)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(37, 55)
-        Me.Label3.TabIndex = 4
-        Me.Label3.Text = "I"
+        Me.lblEigo.AutoSize = True
+        Me.lblEigo.Font = New System.Drawing.Font("Microsoft Sans Serif", 28.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblEigo.Location = New System.Drawing.Point(150, 513)
+        Me.lblEigo.Name = "lblEigo"
+        Me.lblEigo.Size = New System.Drawing.Size(37, 55)
+        Me.lblEigo.TabIndex = 4
+        Me.lblEigo.Text = "I"
         '
-        'Button3
+        'btnNext
         '
-        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button3.Location = New System.Drawing.Point(12, 603)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(100, 29)
-        Me.Button3.TabIndex = 5
-        Me.Button3.Text = "Next"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.btnNext.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNext.Location = New System.Drawing.Point(12, 603)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(100, 29)
+        Me.btnNext.TabIndex = 5
+        Me.btnNext.Text = "Next"
+        Me.btnNext.UseVisualStyleBackColor = True
         '
-        'Button4
+        'btnExit
         '
-        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button4.Location = New System.Drawing.Point(116, 650)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(100, 29)
-        Me.Button4.TabIndex = 6
-        Me.Button4.Text = "Exit"
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(116, 650)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(100, 29)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
         '
-        'TextBox1
+        'txtNihongo
         '
-        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox1.Location = New System.Drawing.Point(93, 272)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(151, 30)
-        Me.TextBox1.TabIndex = 7
+        Me.txtNihongo.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNihongo.Location = New System.Drawing.Point(93, 272)
+        Me.txtNihongo.Name = "txtNihongo"
+        Me.txtNihongo.Size = New System.Drawing.Size(151, 30)
+        Me.txtNihongo.TabIndex = 7
         '
-        'TextBox2
+        'txtEigo
         '
-        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox2.Location = New System.Drawing.Point(93, 466)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(151, 30)
-        Me.TextBox2.TabIndex = 8
+        Me.txtEigo.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtEigo.Location = New System.Drawing.Point(93, 466)
+        Me.txtEigo.Name = "txtEigo"
+        Me.txtEigo.Size = New System.Drawing.Size(151, 30)
+        Me.txtEigo.TabIndex = 8
         '
-        'Button5
+        'btnPrevious
         '
-        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button5.Location = New System.Drawing.Point(220, 603)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(100, 29)
-        Me.Button5.TabIndex = 9
-        Me.Button5.Text = "Previous"
-        Me.Button5.UseVisualStyleBackColor = True
+        Me.btnPrevious.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPrevious.Location = New System.Drawing.Point(220, 603)
+        Me.btnPrevious.Name = "btnPrevious"
+        Me.btnPrevious.Size = New System.Drawing.Size(100, 29)
+        Me.btnPrevious.TabIndex = 9
+        Me.btnPrevious.Text = "Previous"
+        Me.btnPrevious.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(332, 691)
-        Me.Controls.Add(Me.Button5)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Button4)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnPrevious)
+        Me.Controls.Add(Me.txtEigo)
+        Me.Controls.Add(Me.txtNihongo)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnNext)
+        Me.Controls.Add(Me.lblEigo)
+        Me.Controls.Add(Me.btnFlipEigo)
+        Me.Controls.Add(Me.btnFlipNihongo)
+        Me.Controls.Add(Me.lblHiragana)
+        Me.Controls.Add(Me.lblKanji)
         Me.Name = "Form1"
         Me.Text = "フラッシュカード"
         Me.ResumeLayout(False)
@@ -152,14 +152,14 @@ Partial Class Form1
 
     End Sub
 
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button4 As Button
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents Button5 As Button
+    Friend WithEvents lblKanji As Label
+    Friend WithEvents lblHiragana As Label
+    Friend WithEvents btnFlipNihongo As Button
+    Friend WithEvents btnFlipEigo As Button
+    Friend WithEvents lblEigo As Label
+    Friend WithEvents btnNext As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents txtNihongo As TextBox
+    Friend WithEvents txtEigo As TextBox
+    Friend WithEvents btnPrevious As Button
 End Class
